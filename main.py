3b5e30a7bc521f0e1f023603ca76127051641b16
ashok <ashokmommyguy@gmail.com>
2025-07-19 11:37:47 +0530

    modified call duration to 15 mins and changed the logs

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import callscheduler
 
 def main():#This main function is the top level function in the function chain will execute the sub functions
     token = getaccesstoken.get_access_token()
-    # callscheduler.get_untoced_leads(token)
-    # callscheduler.schedule_call_for_accounts(token)
+    callscheduler.get_untoced_leads(token)
+    callscheduler.schedule_call_for_accounts(token)
 
 if __name__ == "__main__":
    main()
